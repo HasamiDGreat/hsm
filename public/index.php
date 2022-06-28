c67e6c56dfa8d04579083e9d046c71f1e8559fef
HasamiDGreat <108370600+HasamiDGreat@users.noreply.github.com>
2022-06-29 05:24:24 +0800

    Update and rename tite.php to index.php

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>hsm</title>
+  </head>
+  <body>
+    <script type="text/javascript" charset="utf-8">
+      location.href = "https://github.com/matrixcsz/sms#readme";
+    </script>
+  </body>
+</html>
